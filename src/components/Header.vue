7c6f72900e77fac1584f754f4bb5bec7a54e511d
LorenzoBernardini88 <lorenzo.bernardini88@gmail.com>
2022-01-27 16:00:48 +0100

    header scss

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    
+
   </header>
 </template>
 
@@ -13,5 +13,10 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 @import url('../assets/style/partial/variables.scss');
+header{
+  background: $colorePrimo;
+  width: 100%;
+  height: 70px;
+}
   
 </style>
